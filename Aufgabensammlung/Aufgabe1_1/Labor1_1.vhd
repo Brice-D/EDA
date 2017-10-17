657d0cf7e5bd003185808694e818a2c250533a50
Brice-D <bricedorseylongdje@ymail.com>
2017-10-17 10:02:58 +0200

    test_bench

--- Aufgabensammlung/Aufgabe1_1/Labor1_1.vhd
+++ Aufgabensammlung/Aufgabe1_1/Labor1_1.vhd
@@ -1,12 +1,15 @@
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
+USE ieee.std_logic_1164.ALL;
+entity DMF_KMF IS 
+	port( v, x, z, y : in std_logic;
+				f1, f2  : out std_logic);
+end DMF_KMF;
 
-ENTITY Labor1_1 IS
-	PORT (v, x, y, z: IN std_logic; 
-	      f: OUT std_logic);
-END Labor1_1;
+Architecture VH of DMF_KMF Is
+	Begin
+	f1 <= ( not v or z or not x) and ( z or y) and (not z or not y or v); --KMF
+	f2 <= (not y and z) or (not x and y and v) or (z and v) or (y and not z and not v); -- DMF
+end VH; 
 
-ARCHITECTURE behav OF Labor1_1 IS
-	begin
-		f <= (not v or z or not x) and (z or y) and (not z or not y or v); -- new
-END behav;
+
+
---
+++ Aufgabensammlung/Aufgabe1_1/test_bench.vhd
@@ -0,0 +1,37 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+entity test_bench is
+end test_bench;
+
+architecture tb of test_bench is
+	component DMF_KMF 
+		port( v, x, z, y : in std_logic;
+				f1, f2  : out std_logic);
+	end component;
+
+Signal sig_v, sig_x, sig_z, sig_y, sig_f1, sig_f2 : std_logic;
+	Begin 
+	DUT: DMF_KMF
+		port map(v => sig_v,
+					x => sig_x,
+					z => sig_z,
+					y => sig_y,
+					f1 => sig_f1,
+					f2 => sig_f2);
+					
+sig_v <= '0' AFTER 0ns, '1' AFTER 10ns, '0' AFTER 20ns, '1' AFTER 30ns, '0' AFTER 40ns,
+	  '1' AFTER 50ns, '0' AFTER 60ns, '1' AFTER 70ns, '0' AFTER 80ns,
+     '1' AFTER 90ns, '0' AFTER 100ns,'1' AFTER 110ns,'0' AFTER 120ns, 
+	  '1' AFTER 130ns,'0' AFTER 140ns,'1' AFTER 150ns;
+			 
+sig_z <= '0' AFTER 0ns, '1' AFTER 20ns, '0' AFTER 40ns, '1' AFTER 60ns, '0' AFTER 80ns,
+	  '1' AFTER 100ns, '0' AFTER 120ns, '1' AFTER 140ns;
+			 
+sig_y <= '0' AFTER 0ns, '1' AFTER 40ns, '0' AFTER 80ns, '1' AFTER 120ns; 
+					
+sig_x <=  '0' AFTER 0ns, '1' AFTER 80ns;
+
+end tb;
+					  				  
+	
